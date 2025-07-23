3eec1c94cedcfdeadaa6adf082d1a8def059d28e
Eric Sorenson <ahpook@github.com>
2025-07-23 10:44:12 -0700

    Include a cool spinner

--- hello.go
+++ hello.go
@@ -1,8 +1,19 @@
 package main
 
-import "github.com/fatih/color"
+import ( 
+    "github.com/fatih/color"
+    "github.com/briandowns/spinner"
+    "time"
+)
 
 func main() {
+    
+    // Create a new spinner with a specific character
+    s := spinner.New(spinner.CharSets[9], 100*time.Millisecond)
+    s.Start()
+    time.Sleep(2 * time.Second)
+    s.Stop()
+
     c := color.New(color.FgCyan).Add(color.Underline).Add(color.BgMagenta)
     c.Println("Dependency graph rulez!")
 }
